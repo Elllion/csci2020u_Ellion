9e41dcba04a41af0e7af2508d8f6bdc787b70bb5
Elllion <32142429+Elllion@users.noreply.github.com>
2018-01-15 16:15:28 -0500

    Merge Fix
    Merged both branches. Method now outputs data from both

--- lab01/test.java
+++ lab01/test.java
@@ -1,10 +1,7 @@
 public class test
 {
 	public static void main(String[] args){
-<<<<<<< HEAD
 		System.out.println("Hello Master!");
-=======
 		System.out.println("Hello Branch!");
->>>>>>> beta
 	}
 }
